dc7abae5e9f6ff713985ef932274e7d0483add26
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2021-03-16 00:22:44 +0600

    Solution: Zigzag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    var result = new Array();
+
+    if (matrix == undefined || matrix.length == 0) {
+        return result;
+    }
+
+    for (var i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                result.push(matrix[i][j]);
+            }
+        } else {
+            for (var j = matrix[i].length - 1; j >= 0; j--) {
+                result.push(matrix[i][j]);
+            }
+        }
+    }
+
+    return result;
+};
